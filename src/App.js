6bd68830e3d110ab15cac895da4cccd4528b9c7a
northross <rossnorthrup@yahoo.com>
2020-08-28 15:23:23 -0400

    refined

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ import Alert from './component/layout/Alert';
 import GithubState from './context/github/GithubState';
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
   const [repos, setRepos] = useState([]);
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
@@ -27,18 +25,6 @@ const App = () => {
   //   this.setState({ users: res.data, loading: false });
   //   console.log(res.data);
   // }
-
-  // get a single user
-  const getUser = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/users/${username}?&client_id=${process.env.REACT_APP_GITHUB_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setUser(res.data);
-    setLoading(false);
-  };
 
   // get users repos
   const getUserRepos = async (username) => {
@@ -80,14 +66,7 @@ const App = () => {
                 exact
                 path='/user/:login'
                 render={(props) => (
-                  <User
-                    {...props}
-                    getUser={getUser}
-                    getUserRepos={getUserRepos}
-                    user={user}
-                    repos={repos}
-                    loading={loading}
-                  />
+                  <User {...props} getUserRepos={getUserRepos} repos={repos} />
                 )}
               />
             </Switch>
